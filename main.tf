31cd84ac1b3a6e978cd91032c6c05cb7a9f2ac2d
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-02-14 19:07:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "google" {
   region      = "us-central1"
 }
 
-resource "google_compute_instance" "jenkinsgcp" {
+resource "google_compute_instance" "gcpjenkins" {
   name         = "terrajenvm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
